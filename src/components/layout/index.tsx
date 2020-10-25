d0b5b13a0cd45803501f81d0358642c21466f8bf
it_dog <it_dog_zhang@163.com>
2020-10-25 23:30:13 +0800

    add route

--- src/components/layout/index.tsx
+++ src/components/layout/index.tsx
@@ -1,4 +1,10 @@
 import React from 'react'
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom"
+import routes from '../../router/index'
 import Header from '../header/index'
 import Slider from '../slider/index'
 
@@ -13,14 +19,32 @@ export default class Zh_Layout extends React.Component{
         <div className="el-header is-primary">
           <Header/>
         </div>
-        <div className="el-container">
-          <div className="el-slider" style={{overflowY:'auto',width: '256px',}}>
-            <Slider></Slider>
-          </div>
-          <div className="el-page" style={{width:'100%'}}>
-
-          </div>
-          </div>
+        <Router>
+          <div className="el-container">
+            <div className="el-slider" style={{overflowY:'auto',width: '256px',}}>
+              <Slider></Slider>
+            </div>
+            <div className="el-page" style={{width:'100%'}}>
+            <Switch>
+                {routes.map((route, index) => (
+                  // You can render a <Route> in as many places
+                  // as you want in your app. It will render along
+                  // with any other <Route>s that also match the URL.
+                  // So, a sidebar or breadcrumbs or anything else
+                  // that requires you to render multiple things
+                  // in multiple places at the same URL is nothing
+                  // more than multiple <Route>s.
+                  <Route
+                    key={index}
+                    path={route.path}
+                    exact={route.exact}
+                    children={<route.sidebar />}
+                  />
+                ))}
+           </Switch>
+            </div>
+            </div>
+          </Router>
       </div>
      )
    }
--- src/components/slider/index.tsx
+++ src/components/slider/index.tsx
@@ -1,4 +1,5 @@
 import React,{ Component } from 'react'
+import { Link } from "react-router-dom";
 import { Menu  } from 'antd'
 import { AppstoreOutlined, MailOutlined, SettingOutlined } from '@ant-design/icons'
 const { SubMenu } = Menu;
@@ -23,8 +24,8 @@ export default class Slider  extends Component{
               }
             >
               <Menu.ItemGroup key="g1" title="Item 1">
-                <Menu.Item key="1">Option 1</Menu.Item>
-                <Menu.Item key="2">Option 2</Menu.Item>
+                <Menu.Item key="1"><Link to="/bubblegum">Option 1</Link></Menu.Item>
+                <Menu.Item key="2"><Link to="/shoelaces">Option 1</Link></Menu.Item>
               </Menu.ItemGroup>
               <Menu.ItemGroup key="g2" title="Item 2">
                 <Menu.Item key="3">Option 3</Menu.Item>
